22adea4bc737e0da7e587f18df0ff97b4fae2696
efirnet <efirnet@gmail.com>
2023-08-18 09:37:55 +0300

    add flag

--- src/components/User/PhoneForm.tsx
+++ src/components/User/PhoneForm.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
+
 import { postUserPhone } from '../../redux/auth/authOperations';
 import { AppDispatch } from '../../redux/store';
 import { Button } from '../Appbar/AppBar.styled';
@@ -23,13 +25,22 @@ const PhoneForm: React.FC<PhoneFormProps> = ({
   const [phoneNumber, setPhoneNumber] = useState(initialPhoneNumber);
   const dispatch: AppDispatch = useDispatch();
 
-  const handleCountryCodeChange = (value: string) => {
-    setCountryCode(value);
-  };
+  // const handleCountryCodeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  //   setCountryCode(e.target.value);
+  // };
+
+  // const handleCountryCodeChange = (value: string) => {
+  //   setCountryCode(value);
+  // };
 
   const handlePhoneNumberChange = (value: string) => {
     setPhoneNumber(value);
   };
+
+  // const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const newPhoneNumber = e.target.value;
+  //   setPhoneNumber(newPhoneNumber);
+  // };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -51,8 +62,8 @@ const PhoneForm: React.FC<PhoneFormProps> = ({
           value={phoneNumber}
           onChange={handlePhoneNumberChange}
           onlyCountries={['ua']}
-          masks={{ ua: '(...) ...-..-..' }}
-          areaCodes={{ ua: ['380'] }}
+          masks={{ ua: '(..) ...-..-..' }}
+          areaCodes={{ ua: ['38'] }}
           localization={{ Ukraine: 'Україна' }}
           inputProps={{
             name: 'phone',
@@ -60,13 +71,19 @@ const PhoneForm: React.FC<PhoneFormProps> = ({
             autoFocus: false,
             style: { width: '100%' },
           }}
+          // countryCodeEditable={false}
+          // onChangeCountry={handleCountryCodeChange}
         />
       </div>
-      <Button style={{ width: '100%' }} type="submit">
+      <Button style={{ width: '100%', marginBottom: 10 }} type="submit">
         {change ? 'Змінити' : 'Встановити'}
       </Button>
       {change && (
-        <Button onClick={() => changeFunc(false)} type="button">
+        <Button
+          style={{ width: '100%' }}
+          onClick={() => changeFunc(false)}
+          type="button"
+        >
           назад
         </Button>
       )}
