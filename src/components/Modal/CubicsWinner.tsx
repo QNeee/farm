f43ed3875d99381a11fd1ec1bd410fb331c622d1
unknown <vovasagan7@gmail.com>
2023-09-23 22:39:24 +0300

    fixCubics

--- src/components/Modal/CubicsWinner.tsx
+++ src/components/Modal/CubicsWinner.tsx
@@ -4,14 +4,29 @@ import { deleteThrowGame } from "../../redux/cubics/cubicsOperations";
 import { getCubicsEndGameResult } from "../../redux/cubics/cubicsSelectors";
 import { BalanceContainer, Container } from "../User/Balance";
 import { Button } from "../Appbar/AppBar.styled";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 
 
 const CubicsWinner = () => {
     const dispatch: AppDispatch = useDispatch();
-    const endGameResult = useSelector(getCubicsEndGameResult)
-    const onClickEndGame = () => {
-        dispatch(deleteThrowGame(''));
+    const { pathname } = useLocation();
+    const endGameResult = useSelector(getCubicsEndGameResult);
+    const cubicKey = 'cubicId';
+    const namePath = pathname.split('/')[1];
+    const [w8, setW8] = useState(false);
+    const onClickEndGame = async () => {
+        if (w8) return;
+        setW8(true);
+        if (namePath === 'demoCubics') {
+            const id = localStorage.getItem(cubicKey);
+            await dispatch(deleteThrowGame(id as string));
+            localStorage.removeItem(cubicKey);
+        } else {
+            await dispatch(deleteThrowGame(''));
+        }
+        setW8(false);
     }
     return <Container><BalanceContainer>{endGameResult.toUpperCase()}
         <Button type="button" onClick={() => onClickEndGame()}>End Game</Button>
